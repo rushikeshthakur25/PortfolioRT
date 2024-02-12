769dbe9e65b4feac49dd9f1d7985283c5aa560c7
rushikeshthakur25 <rushikeshthakur32@gmail.com>
2024-02-12 14:04:42 +0530

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,7 +18,7 @@ export default function About() {
         <div className="py-16 bg-white">
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                    <div className="md:5/12 lg:w-5/12">
+                    <div className="md:5/12 lg:w-5/12 ">
                         <img src={man} alt="image" />
                     </div>
                     <div className="md:7/12 lg:w-6/12">
