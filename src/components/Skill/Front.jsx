aa5106c07592682e019f1a9cd26b034a63f059d2
Rushikesh Thakur <93699961+rushikeshthakur25@users.noreply.github.com>
2024-03-22 10:11:17 +0530

    Update Front.jsx
    add nextjs icon

--- src/components/Skill/Front.jsx
+++ src/components/Skill/Front.jsx
@@ -10,7 +10,9 @@ export default function Front() {
     'React Js': 'React.js - React is an open-source, front-end JavaScript library for building user interfaces or UI components.',
     Tailwind: 'Tailwind CSS - Tailwind is a utility-first CSS framework that provides low-level utility classes to build custom designs.',
     Bootstrap: 'Bootstrap - Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development.',
-    Git:'Git is a distributed version control system used for tracking changes in source code during software development.'
+    Git:'Git is a distributed version control system used for tracking changes in source code during software development.',
+    Next:'Next.js is a React framework for building server-rendered applications. It s often used for creating fast and scalable web applications, particularly those requiring server-side rendering, static site generation, or hybrid rendering.'
+    
 
   });
 
@@ -22,7 +24,8 @@ export default function Front() {
     'React Js': 'https://img.icons8.com/color/48/000000/react-native.png',
     Tailwind: 'https://img.icons8.com/color/48/000000/tailwind-css.png',
     Bootstrap: 'https://img.icons8.com/color/48/000000/bootstrap.png',
-    Git:'https://img.icons8.com/color/48/000000/git.png'
+    Git:'https://img.icons8.com/color/48/000000/git.png',
+    Next:'https://img.icons8.com/color/48/000000/next.png',
 
   });
 
